a8bfdde4ec79481d7867d1bcaff090004edfb697
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2022-04-06 18:33:48 -0300

    resolve ex02

---
+++ fundamentos/bloco-04/dia-01/ex02.js
@@ -0,0 +1,10 @@
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.//
+
+const a = 5;
+const b = 10;
+
+if(a > b){
+    console.log("o maior valor é: "+ a);
+} else {
+    console.log("o maior valor é: "+ b);
+}
