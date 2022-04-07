6c6fb785860545a47559eda4597ce400f8e292cb
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2022-04-07 18:41:58 -0300

    resolvee o ex09

--- fundamentos/bloco-04/dia-02/script.js
+++ fundamentos/bloco-04/dia-02/script.js
@@ -1,11 +1,10 @@
 /*
-Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
 */
 
 let numbers = [];
 
 for (let i = 0; i < 25; i += 1){
     numbers[i] = numbers.push(i);
+    console.log(numbers[i] / 2);
 }
-
-console.log(numbers); 
