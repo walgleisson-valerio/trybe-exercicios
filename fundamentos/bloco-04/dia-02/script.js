a7e506033d9d9037045b1089cb796471f28d05c8
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2022-04-07 18:29:06 -0300

    resolve ex07

--- fundamentos/bloco-04/dia-02/script.js
+++ fundamentos/bloco-04/dia-02/script.js
@@ -1,14 +1,15 @@
 /*
-Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let odd = 0;
+
+let smaller = numbers[0];
 
 for (let number of numbers){
-   if (number % 2 == 1){
-       odd += 1;
-   }
+    if (number < smaller){
+     smaller = number;
+    }
 }
 
-console.log(odd);
+console.log(smaller); 
