094028fff248a30ca03b66b3b9e3eb6d1ae8e254
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2022-04-07 15:47:08 -0300

    resolve ex02

--- fundamentos/bloco-04/dia-02/script.js
+++ fundamentos/bloco-04/dia-02/script.js
@@ -1,9 +1,13 @@
 /*
-Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+let sum = 0;
+
 for (let number of numbers){
-    console.log(number);
+    sum += number;
 }
+
+console.log(sum);
