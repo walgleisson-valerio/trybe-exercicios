8a56be56e47eacc15db9818d4b97acb125226795
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2022-04-07 16:18:18 -0300

    resolve ex05

--- fundamentos/bloco-04/dia-02/script.js
+++ fundamentos/bloco-04/dia-02/script.js
@@ -1,19 +1,14 @@
 /*
-Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let sum = 0;
-
+let bigger = 0;
 for (let number of numbers){
-    sum += number;
+    if (number > bigger){
+        bigger = number;
+    }
 }
 
-let average = sum / numbers.length;
-
-if (average > 20){
-    console.log("valor maior que 20");
-}else{
-    console.log("valor menor ou igual a 20");
-}
+console.log(bigger);
