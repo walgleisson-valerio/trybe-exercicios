6384007f2e7dba0d5393d429ea664d89f77c7da6
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2022-04-07 15:56:23 -0300

    resolve ex04

--- fundamentos/bloco-04/dia-02/script.js
+++ fundamentos/bloco-04/dia-02/script.js
@@ -1,5 +1,5 @@
 /*
-Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
@@ -12,4 +12,8 @@ for (let number of numbers){
 
 let average = sum / numbers.length;
 
-console.log(average);
+if (average > 20){
+    console.log("valor maior que 20");
+}else{
+    console.log("valor menor ou igual a 20");
+}
