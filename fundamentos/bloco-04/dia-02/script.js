9b0bf0dfcc246970024c480c9ebc04afc8450674
walgleisson valerio <walgleissonvaleriodev@gmail.com>
2022-04-07 18:22:08 -0300

    resolve ex06

--- fundamentos/bloco-04/dia-02/script.js
+++ fundamentos/bloco-04/dia-02/script.js
@@ -1,14 +1,14 @@
 /*
-Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let odd = 0;
 
-let bigger = 0;
 for (let number of numbers){
-    if (number > bigger){
-        bigger = number;
-    }
+   if (number % 2 == 1){
+       odd += 1;
+   }
 }
 
-console.log(bigger);
+console.log(odd);
